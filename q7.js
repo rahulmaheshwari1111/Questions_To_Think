fa5c03c1c992326ed562fb46ee7dc4becc625dd7
rahulmaheshwari1111 <rahulmaheshwari240@gmail.com>
2021-03-28 22:28:06 +0530

    easy

---
+++ q7.js
@@ -0,0 +1,20 @@
+// What will the code below output to the console and why ?
+
+console.log(1 +  "2" + "2");
+console.log(1 +  +"2" + "2");
+console.log(1 +  -"1" + "2");
+console.log(+"1" +  "1" + "2");
+console.log( "A" - "B" + "2");
+console.log( "A" - "B" + 2);
+
+
+
+
+
+//answer
+// "122"
+// "32"
+// "02"
+// "112"
+// "NaN2"
+// NaN
