6c25bd013cb034ca56c051d7d9e6ede8ab0df299
rahulmaheshwari1111 <rahulmaheshwari240@gmail.com>
2021-03-28 22:24:59 +0530

    q2 - oops logic

---
+++ q2.js
@@ -0,0 +1,32 @@
+// What will be the output and why?
+
+
+var myObject = {
+    foo: "bar",
+    func: function() {
+        var self = this;
+        console.log("outer func:  this.foo = " + this.foo);
+        console.log("outer func:  self.foo = " + self.foo);
+        (function() {
+            console.log("inner func:  this.foo = " + this.foo);
+            console.log("inner func:  self.foo = " + self.foo);
+        }());
+    }
+};
+myObject.func();
+
+
+// answer:   bar / bar/undefined/bar
+
+
+
+
+
+
+// reason: In the outer function, both this and self refer to myObject and therefore both can properly reference and access foo.
+
+//In the inner function, though, this no longer refers to myObject. 
+//As a result, this.foo is undefined in the inner function, whereas the reference to the local variable self remains in scope and is accessible there.
+
+
+
