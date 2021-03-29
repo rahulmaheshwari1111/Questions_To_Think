4a7c5e929464a693fa4f8c3a0779ac561f6ae164
rahulmaheshwari1111 <rahulmaheshwari240@gmail.com>
2021-03-29 23:57:12 +0530

    added q 13

---
+++ q13.js
@@ -0,0 +1,32 @@
+// Consider the following code. What will the output be, and why?
+
+(function () {
+    try {
+        throw new Error();
+    } catch (x) {
+        var x = 1, y = 2;
+        console.log(x);
+    }
+    console.log(x);
+    console.log(y);
+})();
+
+
+// 1
+// undefined
+// 2
+// var statements are hoisted (without their value initialization) to the top of the global or function scope it belongs to, 
+// even when it’s inside a with or catch block. However, the error’s identifier is only visible inside the catch block. It is equivalent to:
+
+(function () {
+    var x, y; // outer and hoisted
+    try {
+        throw new Error();
+    } catch (x /* inner */) {
+        x = 1; // inner x, not the outer one
+        y = 2; // there is only one y, which is in the outer scope
+        console.log(x /* inner */);
+    }
+    console.log(x);
+    console.log(y);
+})();
