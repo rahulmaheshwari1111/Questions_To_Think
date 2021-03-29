a7190db73fac12c4459ac45b677f29027ca7888e
rahulmaheshwari1111 <rahulmaheshwari240@gmail.com>
2021-03-29 23:57:34 +0530

    added q 15

---
+++ q15.js
@@ -0,0 +1,5 @@
+//What is the value of typeof undefined == typeof NULL?
+
+// The expression will be evaluated to true, since NULL will be treated as any other undefined variable.
+
+// Note: JavaScript is case-sensitive and here we are using NULL instead of null.
